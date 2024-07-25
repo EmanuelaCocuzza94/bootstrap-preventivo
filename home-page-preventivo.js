c57608ff967a0bd77a0570dc1766e817935af75b
EmanuelaCocuzza <emauelacocuzza94@gmail.com>
2024-07-25 18:27:25 +0200

    continuazione-js-2

--- home-page-preventivo.js
+++ home-page-preventivo.js
@@ -16,6 +16,8 @@ const sconto = document.getElementById('sconto');
 
 const privacy = document.getElementById('privacy');
 
+const tabellaRisultato = document.getElementById('tabella-risultato');
+
 
 preventivo.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -25,8 +27,10 @@ preventivo.addEventListener('submit', function(e) {
 
     const oreLavoro = 10;
     
-    let prezzo = 0 ;
-    console.log(prezzo);
+    let prezzo = 0;
+    let sconto_applicato = 0;
+    //console.log(prezzo);
+
 
     if(tipoLavoro.value === 'backend') {
         prezzo = 20.50 * oreLavoro;
@@ -37,26 +41,59 @@ preventivo.addEventListener('submit', function(e) {
 
     } else if (tipoLavoro.value === 'analisi') {
         prezzo =  33.60 * oreLavoro;
-    };
+    }
 
     //inserire lo sconto con un array.
+    let codiceSconto = [
+        'YHDNU32',
+        'JANJC63',
+        'PWKCN25',
+        'SJDPO96',
+        'POCIE24',
+    ];
+    
+    //caso 1: inserisce un codice sconto sbagliato if (...== 0 && sconto.value != '') 
+    //caso 2: non inserisce un codice sconto => quando sconto == 0 (...== 0 && sconto.value == '')
+
+    for (let i = 0 ; i < codiceSconto.length; i++) {
+        console.log(sconto.value)
+        console.log(codiceSconto[i], 'Codice Sconto')
+
+        if(sconto.value === codiceSconto[i]) {
+
+            sconto_applicato = (prezzo * 25/100);
+            
+            prezzo = prezzo -  sconto_applicato;
+
+        }
+
+    }
+
+    if(sconto_applicato === 0 && sconto.value != '') {
+        sconto_applicato = 'Codice sconto non valido'
+    }
+
+    tabellaRisultato.removeAttribute('style');
+
+    //tabellaRisultato.style.display = 'show';
 
 
+    
     console.log('prezzo: ', prezzo);
 
-    console.log(preventivo.value, nome.value, cognome.value, email.value, tipoLavoro.value, messaggio.value, sconto.value, privacy.value);
+   // console.log(preventivo.value, nome.value, cognome.value, email.value, tipoLavoro.value, messaggio.value, sconto.value, privacy.value);
     
-    document.getElementById('prezzo').innerHTML = prezzo;
-
-    document.getElementById('nome-preventivo').innerHTML = nome.value;
-
-    document.getElementById('cognome-preventivo').innerHTML = cognome.value;
+    document.getElementById('prezzo').innerHTML = prezzo.toFixed(2) + '€';
 
     document.getElementById('email-preventivo').innerHTML = email.value;
 
     document.getElementById('tipo-preventivo').innerHTML = tipoLavoro.value;
 
-    document.getElementById('messaggio-preventivo').innerHTML = messaggio.value;
+    document.getElementById('sconto-preventivo').innerHTML = sconto.value;
+
+    document.getElementById('sconto-applicato').innerHTML = sconto_applicato;
+
+
 
 
     
